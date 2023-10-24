95a67c33ef81b969729d2de8f4be41718d805f71
OviedoF <oviedofederico39@gmail.com>
2023-10-24 17:09:45 -0300

    Elección de botones en menú captura

--- pages/CaptureMenu.jsx
+++ pages/CaptureMenu.jsx
@@ -1,14 +1,36 @@
 import { View, Text, Switch } from "react-native";
 import { Link, useNavigate } from "react-router-native";
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import styles from "../styles/styles";
 import SectionBar from "../components/SectionBar";
 import routes from "../router/routes";
+import RadioGroup from 'react-native-radio-buttons-group';
 
 const CaptureMenu = () => {
   const navigate = useNavigate();
   const [isEnabled, setIsEnabled] = useState(false);
+  const [selectedId, setSelectedId] = useState(3)
   const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
+  const optionsRadio = useMemo(() => ([
+    {
+      id: 1,
+      label: 'Ingreso 1 a 1',
+      value: 'single'
+    },
+    {
+      id: 2,
+      label: 'Ingreso por cantidad',
+      value: 'multiple'
+    },
+    {
+      id: 3,
+      label: 'Ambos',
+      value: 'both',
+      default: true
+    }
+  ]), [])
+
   return (
     <>
       <View style={styles.topSectionContainer}>
@@ -17,22 +39,30 @@ const CaptureMenu = () => {
         </Text>
       </View>
       <SectionBar section={"Menu Captura"} backTo={routes.home} />
+
       <View style={styles.container}>
-        <Link to={routes.singleProductEntry} style={styles.primaryBtn}>
-          <View style={{ flexDirection: "row", justifyContent: "center" }}>
-            <Text style={styles.white}>INGRESO 1 A 1</Text>
-          </View>
-        </Link>
-        <Link to={routes.multipleProductEntry} style={styles.primaryBtn}>
-          <View style={{ flexDirection: "row", justifyContent: "center" }}>
-            <Text style={styles.white}>INGRESO POR CANTIDAD</Text>
-          </View>
-        </Link>
+        {selectedId === 1 || selectedId === 3 ? (
+          <Link to={routes.singleProductEntry} style={styles.primaryBtn}>
+            <View style={{ flexDirection: "row", justifyContent: "center" }}>
+              <Text style={styles.white}>INGRESO 1 A 1</Text>
+            </View>
+          </Link>
+        ) : null}
+
+        {selectedId === 2 || selectedId === 3 ? (
+          <Link to={routes.multipleProductEntry} style={styles.primaryBtn}>
+            <View style={{ flexDirection: "row", justifyContent: "center" }}>
+              <Text style={styles.white}>INGRESO POR CANTIDAD</Text>
+            </View>
+          </Link>
+        ) : null}
+
         <Link to={routes.home} style={styles.primaryBtn}>
           <View style={{ flexDirection: "row", justifyContent: "center" }}>
             <Text style={styles.white}>ENVIAR CONTEO WIFI</Text>
           </View>
         </Link>
+
         <View
           style={{
             display: "flex",
@@ -49,6 +79,13 @@ const CaptureMenu = () => {
             value={isEnabled}
           />
         </View>
+
+        <RadioGroup
+          radioButtons={optionsRadio}
+          onPress={setSelectedId}
+          selectedId={selectedId}
+          layout="row"
+        />
       </View>
     </>
   );
