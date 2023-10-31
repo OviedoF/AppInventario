77f622fd1ffc4b52849d9b4e3a1b7ce1d33171db
Ivan Correa <ivaanmc95@gmail.com>
2023-10-31 16:15:05 -0300

    Agrego funciones para manejar carrito en dataContext

--- context/DataContext.jsx
+++ context/DataContext.jsx
@@ -4,12 +4,53 @@ export const dataContext = createContext();
 
 export const DataProvider = ({ children }) => {
   const [area, setArea] = useState(undefined);
-
+  /* const [cart, setCart] = useState([]);
+  const [cartCant, setCartCant] = useState();
+  //Verificar si esta en carrito
+  const isInCartContext = (name) => {
+    return cart.some((item) => item.item === name);
+  };
+  //Agregar al carrito
+  const addToCart = (name, cant) => {
+    if (isInCartContext(name)) {
+      // El producto ya está en el carrito, actualiza la cantidad.
+      const updatedCart = cart.map((item) => {
+        if (item.item === name) {
+          return { item: name, cant: item.cant + cant };
+        }
+        return item;
+      });
+      setCart(updatedCart);
+    } else {
+      // El producto no está en el carrito, entonces lo agrega.
+      setCart([...cart, { item: name, cant }]);
+    }
+    setCartCant(totalItemsInCart());
+  };
+  //Limpiar carrito
+  const clearCart = () => {
+    setCart([]);
+  };
+  //Total carrito
+  const totalItemsInCart = () => {
+    let total = 0;
+    cart.forEach((item) => (total = total + item.cant));
+    return total;
+  };
+  //Borrar item
+  const eraseItemFromCart = (name) => {
+    let newCart = cart.filter((item) => item !== name);
+    setCart(newCart);
+  }; */
   return (
     <dataContext.Provider
       value={{
         area,
         setArea,
+        /* cartCant,
+        addToCart,
+        clearCart,
+        eraseItemFromCart, */
       }}
     >
       {children}
---
+++ pages/Review.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Review = () => {
+  //? Lista de productos agregados al area
+  return (
+    <View>
+      <Text>Review</Text>
+    </View>
+  );
+};
+
+export default Review;
